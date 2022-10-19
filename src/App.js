175fcd98e8278ba7bcca4a231e9c5326e3a87baa
Shadow <shadowdemonics@gmail.com>
2022-10-20 03:36:36 +0500

    GG
    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App () {
+    const [calculation, setCalculation] = useState("");
+    const [result, setResult ] =useState("");
+    const operators =['/','*','+','-','.',]
+    
+    
+const createDigits= () => {
+    const digits=[];
+    for (let i = 1; i < 10;i++){
+        digits.push(
+            <button 
+            onClick={() => updatecalculation(i.toString())}
+            key={i}>
+                {i}
+            </button>
+        )
+}
+return digits;
+}
+const deleteLast = () => {
+    if (calculation == '') {
+        return;
+    }
+    const value = calculation.slice(0, -1 );
+    setCalculation(value);
+}
+const updatecalculation = value => {
+    setCalculation(calculation+value);
+  
+setCalculation(calculation+value);
+
+if (!operators.includes(value)) {
+    setResult(eval(calculation+value).toString());
 }
 
+
+}
+const calculate = () => {
+    setCalculation(eval(calculation).toString())
+}
+  return (
+      <div className="App">
+          <div className="calculator">
+          <div className="Display">     
+                  {result ? <span>({result})</span>: ''} {calculation || "0"}
+              </div>
+              <div className="operators"> 
+              <button onClick={() => updatecalculation('/')} >/</button>
+              <button onClick={() => updatecalculation('*')}>*</button>
+              <button onClick={() => updatecalculation('+')}>+</button>
+              <button onClick={() => updatecalculation('-')}>-</button>
+
+              <button onClick={deleteLast}>DEL</button>
+              </div>
+              <div className="digits">
+                  { createDigits() }
+                 
+              <button onClick={() => updatecalculation('0')}>0</button>
+              <button onClick={() => updatecalculation('.')} >.</button>
+              <button onClick={calculate} >=</button>
+              
+
+              
+              </div>
+
+              </div>
+              </div>
+
+
+
+  );
+  }
 export default App;
+
+
